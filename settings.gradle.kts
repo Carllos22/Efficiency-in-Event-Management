b67adc54a625ab1aa8177f8d75d991f0676b467f
Carlos <carloscontreras9522@gmail.com>
2024-07-08 10:35:17 +0200

    Empty project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Efficiency_in_Event_Management"
+include(":app")
